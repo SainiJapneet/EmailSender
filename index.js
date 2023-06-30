a9356256b6b0c8bba95d36f33e6eafbe0bdb76b3
japneet <japneetsaini084@gmail.com>
2023-06-30 09:44:39 +0530

    email sender completed

---
+++ index.js
@@ -0,0 +1,26 @@
+const nodemailer = require('nodemailer')
+
+const cred = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: 'dummy_mail@gmail.com',
+        pass: 'dummy_password'
+    }
+});
+
+const mailInfo = {
+    from: 'dummy_mail@gmail.com',
+    to: 'dummy_receiver@gmail.com',
+    subject: 'Testing mail',
+    text: '... :-)'
+};
+
+cred.sendMail(
+    mailInfo, (err, info)=>{
+        if(err){
+            console.log("Something went wrong : " + err)
+        }else{
+            console.log("Email sent : " + info.response)
+        }
+    }
+)
